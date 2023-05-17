053cac04f7b5368e812e82baae499b166d58e2e2
Ievgennii <simonovievgenii@gmail.com>
2023-05-17 23:32:32 +0300

    ImageGalleryItem

--- src/components/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem.jsx
@@ -1,27 +1,48 @@
-import React, { Component } from 'react';
+import React from 'react';
 import css from './styles.module.css';
 
-class ImageGalleryItem extends Component {
-  handleClick = largeImageURL => {
-    this.props.onImageClick(largeImageURL);
+const ImageGalleryItem = ({ images, onImageClick }) => {
+  const handleClick = (largeImageURL) => {
+    onImageClick(largeImageURL);
   };
-  render() {
-    const { images } = this.props;
-    return (
-      <>
-        {images.map(({ id, webformatURL, largeImageURL, tags }) => (
-          <li key={id} className={css.ImageGalleryItem}>
-            <img
-              className={css.ImageGalleryItemImage}
-              src={webformatURL}
-              alt={tags}
-              onClick={() => this.handleClick(largeImageURL)}
-            />
-          </li>
-        ))}
-      </>
-    );
-  }
-}
+
+  return (
+    <>
+      {images.map(({ id, webformatURL, largeImageURL, tags }) => (
+        <li key={id} className={css.ImageGalleryItem}>
+          <img
+            className={css.ImageGalleryItemImage}
+            src={webformatURL}
+            alt={tags}
+            onClick={() => handleClick(largeImageURL)}
+          />
+        </li>
+      ))}
+    </>
+  );
+};
+
+// class ImageGalleryItem extends Component {
+//   handleClick = largeImageURL => {
+//     this.props.onImageClick(largeImageURL);
+//   };
+//   render() {
+//     const { images } = this.props;
+//     return (
+//       <>
+//         {images.map(({ id, webformatURL, largeImageURL, tags }) => (
+//           <li key={id} className={css.ImageGalleryItem}>
+//             <img
+//               className={css.ImageGalleryItemImage}
+//               src={webformatURL}
+//               alt={tags}
+//               onClick={() => this.handleClick(largeImageURL)}
+//             />
+//           </li>
+//         ))}
+//       </>
+//     );
+//   }
+// }
 
 export default ImageGalleryItem;
