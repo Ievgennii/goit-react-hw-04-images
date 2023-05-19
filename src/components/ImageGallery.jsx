0ea8543148d39edc9e50ca4c6adfaf4754cf97f8
Ievgennii <simonovievgenii@gmail.com>
2023-05-19 21:58:18 +0300

    comcom

--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -1,7 +1,3 @@
-
-
-
-// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 import React, { useState, useEffect } from 'react';
 // import React, { Component } from 'react';
 import ImageGalleryItem from './ImageGalleryItem';
@@ -18,53 +14,16 @@ const ImageGallery = ({ search }) => {
   const [page, setPage] = useState(1);
   const [selectedImage, setSelectedImage] = useState(null);
   const [totalHits, setTotalHits] = useState(null);
-  
-  useEffect(() => {
-    // setPage(1)
-    // console.log(page)
-    const fetchData = async () => {
-      if (!search) {
-        return;
-      }
-      setIsLoading(true);
-
-      try {
-        const result = await getImagesWithQuery(search, 1);
-        const { hits, totalHits } = result;
-
-        setImages(hits);
-        setTotalHits(totalHits);
-        console.log('++++++++++++++++++++++++++++++++');
-        setPage(1);
-        if (hits.length === 0) {
-          setTimeout(() => {
-            alert("We're sorry, but we didn't find anything for your request.");
-          }, 200);
-        }
-      } catch (error) {
-        setError(error);
-        alert(`Whoops, something went wrong: ${error.message}`);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    fetchData();
-  }, [search]);
-
-
 
   useEffect(() => {
-    
     const fetchData1 = async () => {
-      
       if (page !== 1) {
         setIsLoading(true);
-        console.log(page);
+        console.log(`page ${page}`);
         try {
           const result = await getImagesWithQuery(search, page);
           const { hits } = result;
-          console.log('_____________________________________');
+          // console.log('_____________________________________');
           setImages(prevImages => [...prevImages, ...hits]);
         } catch (error) {
           setError(error);
@@ -76,7 +35,39 @@ const ImageGallery = ({ search }) => {
     };
 
     fetchData1();
-  }, [page]);
+  }, [search, page]);
+
+  useEffect(() => {
+    setPage(1);
+    // console.log(page)
+    const fetchData = async () => {
+      if (!search) {
+        return;
+      }
+      setIsLoading(true);
+
+      try {
+        const result = await getImagesWithQuery(search, 1);
+        setTimeout(() => {
+          const { hits, totalHits } = result;
+          setImages(hits);
+          setTotalHits(totalHits);
+          // console.log('++++++++++++++++++++++++++++++++');
+          // setPage(1);
+          if (hits.length === 0) {
+            alert("We're sorry, but we didn't find anything for your request.");
+          }
+        }, 200);
+      } catch (error) {
+        setError(error);
+        alert(`Whoops, something went wrong: ${error.message}`);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [search]);
 
   const changePage = () => {
     if (images.length > 0) {
@@ -116,117 +107,3 @@ const ImageGallery = ({ search }) => {
 };
 
 export default ImageGallery;
-// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-// class ImageGallery extends Component {
-//   state = {
-//     images: [],
-//     isLoading: false,
-//     error: null,
-//     page: 1,
-//     search: '',
-//     selectedImage: null,
-//     totalHits: null,
-//   };
-
-//   async componentDidUpdate(prevProps, prevState) {
-//     if (prevProps.search !== this.props.search) {
-//       this.setState({ isLoading: true });
-//       this.setState({ search: this.props.search });
-
-//       try {
-//         const images = await getImagesWithQuery(this.props.search, 1);
-
-//         this.setState({
-//           images: images.hits,
-//           emptyResponce: !images.hits.length,
-//           totalHits: images.totalHits,
-//         });
-//       } catch (error) {
-//         this.setState({ error });
-//         alert(`Whoops, something went wrong: ${error.message}`);
-//       } finally {
-//         this.setState({ isLoading: false });
-//       }
-//     }
-
-//     if (prevState.page !== this.state.page && this.state.page !== 1) {
-//       this.setState({ isLoading: true });
-//       this.setState({ search: this.props.search });
-
-//       try {
-//         const images = await getImagesWithQuery(
-//           this.props.search,
-//           this.state.page
-//         );
-//         this.setState(prevState => ({
-//           images: [...prevState.images, ...images.hits],
-//         }));
-//       } catch (error) {
-//         this.setState({ error });
-//         alert("We're sorry, but you've reached the end of search results.");
-//       } finally {
-//         this.setState({ isLoading: false });
-//       }
-//     }
-//   }
-
-//   changePage = () => {
-//     if (this.state.images.length > 0) {
-//       this.setState(prevState => ({
-//         page: prevState.page + 1,
-//       }));
-//     }
-//     this.setState(prevState => ({
-//       totalHits: prevState.totalHits - 12,
-//     }));
-
-//     if (this.state.totalHits < 24) {
-//       setTimeout(() => {
-//         alert("We're sorry, but you've reached the end of search results.");
-//       }, 1000);
-//     }
-//   };
-
-//   handleImageClick = largeImageURL => {
-//     this.setState({ selectedImage: largeImageURL });
-//   };
-
-//   render() {
-//     const {
-//       images,
-//       isLoading,
-//       error,
-//       emptyResponce,
-//       selectedImage,
-//       totalHits,
-//     } = this.state;
-
-//     return (
-//       <div>
-//         <ul className={css.ImageGallery}>
-//           {isLoading && <MagnifyingGlass />}
-//           {emptyResponce && (
-//             <p>We're sorry, but we didn't find anything for your request.</p>
-//           )}
-
-//           {images.length > 0 && (
-//             <ImageGalleryItem
-//               images={images}
-//               onImageClick={this.handleImageClick}
-//             />
-//           )}
-//         </ul>
-//         <div className={css.ButtonConteiner}>
-//           {!error && totalHits > 12 && <Button changePage={this.changePage} />}
-//         </div>
-//         {selectedImage && (
-//           <Modal onClose={() => this.setState({ selectedImage: null })}>
-//             <img src={selectedImage} alt="" />
-//           </Modal>
-//         )}
-//       </div>
-//     );
-//   }
-// }
-
-// export default ImageGallery
